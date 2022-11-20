cd383855ff094b8c17e5626c3b74a248d50321a3
sansito84 <santiagosito@gmail.com>
2022-11-19 22:41:29 -0300

    Calling API nice

--- APIcall.js
+++ APIcall.js
@@ -1,18 +1,24 @@
-(document).ready(function() {
+let weatherLoc = "https://weather-proxy.freecodecamp.rocks/api/current?";
+
+document.addEventListener("DOMContentLoaded", function() {
     if (navigator.geolocation) {
-        getLocalization();
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var lat = "lat=" + position.coords.latitude;
+            var lon = "lon=" + position.coords.longitude;
+            var acc = "acc=" + position.coords.accuracy;
+            console.log(lat, lon, acc);
+            let getWeather = weatherLoc + lat + "&" + lon;
+            console.table(getWeather);
+            getWeatherAPI(getWeather);
+        });
     } else {
         console.log("Geolocation is not supported by this browser.");
     }
-})
+});
 
-function getLocalization() {
-    var weatherLoc = "https://weather-proxy.freecodecamp.rocks/api/current?"
-    navigator.geolocation.getCurrentPosition(function(position) {
-        var lat = "lat=" + position.coords.latitude;
-        var lon = "lon=" + position.coords.longitude;
-        console.log(lat, lon);
-        var getWeather = weatherLoc + lat + "&" + lon;
-        console.log(getWeather);
-    });
+function getWeatherAPI(getWeather) {
+    fetch(getWeather)
+        .then((response) => response.json())
+        .then((json) => console.log(json));
 }
+document.getElementById('acc')
