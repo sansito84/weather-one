bc21b791ae639b0bf1831076dbd05fd53eda9c88
sansito84 <santiagosito@gmail.com>
2022-11-26 05:51:22 -0300

    Funcionando fetch y renderizando valores en pagina

--- APIcall.js
+++ APIcall.js
@@ -1,14 +1,14 @@
-let weatherLoc = "https://weather-proxy.freecodecamp.rocks/api/current?";
+const weatherLoc = "https://weather-proxy.freecodecamp.rocks/api/current?";
 
 document.addEventListener("DOMContentLoaded", function() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
-            var lat = "lat=" + position.coords.latitude;
-            var lon = "lon=" + position.coords.longitude;
-            var acc = "acc=" + position.coords.accuracy;
+            const lat = "lat=" + position.coords.latitude;
+            const lon = "lon=" + position.coords.longitude;
+            const acc = "acc=" + position.coords.accuracy;
             console.log(lat, lon, acc);
             let getWeather = weatherLoc + lat + "&" + lon;
-            console.table(getWeather);
+            console.log(getWeather);
             getWeatherAPI(getWeather);
         });
     } else {
@@ -18,7 +18,27 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function getWeatherAPI(getWeather) {
     fetch(getWeather)
-        .then((response) => response.json())
-        .then((json) => console.log(json));
+        .then((res) => res.json())
+        .then((data) => getDatos(data));
 }
-document.getElementById('acc')
+
+function getDatos(dato) {
+    const temp = document.createElement("h2");
+    temp.textContent = dato.main.temp + " ºC";
+    document.getElementById("temp").appendChild(temp);
+    const speed = document.createElement("h2");
+    speed.textContent = dato.wind.speed + " m/s";
+    document.getElementById("speed").appendChild(speed);
+    const weather = document.createElement("h3");
+    weather.textContent = dato.weather[0].main;
+    document.getElementById("weather").appendChild(weather);
+    const name = document.createElement("h1");
+    name.textContent = dato.name;
+    document.getElementById("name").appendChild(name);
+    const country = document.createElement("h1");
+    country.textContent = dato.sys.country;
+    document.getElementById("country").appendChild(country);
+    const feelslike = document.createElement("h2");
+    feelslike.textContent = dato.main.feels_like + " ºC";
+    document.getElementById("feelslike").appendChild(feelslike);
+}
