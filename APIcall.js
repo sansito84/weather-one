895d1df51f581d5f7cac738991f34a4a5cd75bd4
sansito84 <santiagosito@gmail.com>
2022-11-28 12:57:44 -0300

    agregados los iconos

--- APIcall.js
+++ APIcall.js
@@ -1,6 +1,5 @@
 const weatherLoc = "https://weather-proxy.freecodecamp.rocks/api/current?";
 
-
 document.addEventListener("DOMContentLoaded", function() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
@@ -32,7 +31,7 @@ function getDatos(dato) {
     const speed = document.createElement("h2");
     speed.textContent = dato.wind.speed + " m/s";
     document.getElementById("speed").appendChild(speed);
-    const weather = document.createElement("h3");
+    const weather = document.createElement("h2");
     weather.textContent = dato.weather[0].main;
     document.getElementById("weather").appendChild(weather);
     const name = document.createElement("h1");
@@ -45,49 +44,71 @@ function getDatos(dato) {
     feelslike.textContent = Math.round(dato.main.feels_like) + " ºC";
     document.getElementById("feelslike").appendChild(feelslike);
     backgroundGenerator();
+    // iconGen(weather)
 }
 
 function backgroundGenerator() {
     var container = document.getElementById("main");
     const clima = document.getElementById("weather").textContent;
+    var weather = document.getElementById("weather");
+
     console.log(container, clima);
     if (clima === "Clouds") {
-        container.style.backgroundImage = "url('https://i.pinimg.com/originals/c1/f0/09/c1f009b219a31b387765b9a7bd8f6f1b.gif')";
+        container.style.backgroundImage =
+            "url('https://i.pinimg.com/originals/c1/f0/09/c1f009b219a31b387765b9a7bd8f6f1b.gif')";
         container.style.color = "mediumgray";
+        weather.setAttribute("icon", "ph:cloud-sun-bold");
+        weather.innerText = "";
     }
     if (clima === "Rain") {
-        container.style.backgroundImage = "url('https://www.gannett-cdn.com/-mm-/09629241d104a4756c279bf1ea4294534f1d70f1/c=0-59-640-421/local/-/media/2016/10/31/INGroup/Evansville/636135276888682113-sunny1.jpg?width=1200&disable=upscale&format=pjpg&auto=webp')";
+        container.style.backgroundImage =
+            "url('https://www.gannett-cdn.com/-mm-/09629241d104a4756c279bf1ea4294534f1d70f1/c=0-59-640-421/local/-/media/2016/10/31/INGroup/Evansville/636135276888682113-sunny1.jpg?width=1200&disable=upscale&format=pjpg&auto=webp')";
+        weather.setAttribute("icon", "ph:sun-bold");
+        weather.innerText = "";
     }
     if (clima === "Snow") {
-        container.style.backgroundImage = "url('https://i.gifer.com/3Pm1.gif')"
+        container.style.backgroundImage = "url('https://i.gifer.com/3Pm1.gif')";
         container.style.color = "grey";
+        weather.setAttribute("icon", "ph:sun-bold");
+        weather.innerText = "";
     }
     if (clima === "Hail") {
-        container.style.backgroundImage = "url('https://media.tenor.com/RO0pokQ2mwgAAAAC/hail-hail-storm.gif')";
+        container.style.backgroundImage =
+            "url('https://media.tenor.com/RO0pokQ2mwgAAAAC/hail-hail-storm.gif')";
         container.style.color = "white";
+        weather.setAttribute("icon", "ph:sun-bold");
+        weather.innerText = "";
     }
     if (clima === "Fog") {
-        container.style.backgroundColor = "green";
+        container.style.backgroundImage =
+            "url('https://media.tenor.com/hN8ma5kfmF0AAAAC/fog-mountains.gif')";
         container.style.color = "white";
+        weather.setAttribute("icon", "ph:sun-bold");
+        weather.innerText = "";
     }
     if (clima === "Thunderstom") {
-        container.style.backgroundImage = "url('https://i.gifer.com/KNUi.gif')"
+        container.style.backgroundImage = "url('https://i.gifer.com/KNUi.gif')";
         container.style.color = "white";
+        weather.setAttribute("icon", "ph:sun-bold");
+        weather.innerText = "";
     }
     if (clima === "Clear") {
-        container.style.backgroundImage = "url('https://data.whicdn.com/images/281744156/original.gif')";
-        container.style.color = "light gray";
+        container.style.backgroundImage =
+            "url('https://data.whicdn.com/images/281744156/original.gif')";
+        container.style.color = "lightcyan";
+        weather.setAttribute("icon", "ph:sun-bold");
+        weather.innerText = "";
     }
 }
 
-// create a function to change background color of element with Id="main" deppending of Id="weather" innerText value
-// function changeElement(element, div) {
-//     console.log(element.innerText)
-//     if ((element.innerText === "Rain")) {
-//         div.style.backgroundColor = "red";
+// function iconGen(weather) {
+//     var container = document.getElementById("main");
+//     const clima = document.getElementById("weather").textContent;
+//     var weather = document.getElementById("weather");
+//     console.log(container, clima);
+//     if (clima === "Clouds") {;
 //     }
-//     if ((element.innerText === "Clouds")) {
-//         div.style.backgroundColor = "gray";
-//     }
+//     if (clima === "Clear") {
 
+//     }
 // }
